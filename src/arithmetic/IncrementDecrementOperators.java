31c9cec01e0b9ccf26471208f668ce2cd481cb22
Nigel Wilkerson <njw22001@gmail.com>
2026-05-20 00:10:48 +0900

    Add IncrementDecrementOperators - demonstrate ++ and -- operators

--- src/arithmetic/IncrementDecrementOperators.java
+++ src/arithmetic/IncrementDecrementOperators.java
@@ -1,16 +1,32 @@
 package arithmetic;
+
 /**
  * Name: Nigel Wilkerson
  * File: IncrementDecrementOperators.java
- * Version: 
+ * Version: 1.0
  * Date: 5/19/2026
- * Description:
+ * Description: Demonstrates increment (++) and decrement (--) operators, shortcuts used to add
+ * or subtract 1 from a variable, commonly used in loops and counters
  */
 
 public class IncrementDecrementOperators {
 
     public static void main(String[] args) {
-    
-        System.out.println("Program started");
+
+        // Increment & Decrement Operators (only addition and subtraction
+
+        System.out.println("Below are practice examples of increment and decrement assignment operators:\n");
+
+        int x = 1;
+        int y = 1;
+
+        //Addition
+        x++;
+        System.out.println("After x++, x is now: " + x);
+
+        //Subtraction
+        y--;
+        System.out.println("After y--, x is now: " + y);
     }
 }
+
