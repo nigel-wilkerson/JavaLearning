10ad15079d1e70f0044bfa64569a2657c7622f51
Nigel Wilkerson <njw22001@gmail.com>
2026-07-13 07:42:15 +0900

    Refactor ArrayUserInput to use return value pattern
    Refactored getMovieCount() to return an int instead of modifying a class-level variable. This makes the method's purpose explicit in its signature and eliminates hidden side effects. Follows the pattern of compute-and-return instead of modify-shared-state.

--- src/arrayUserInput/ArrayUserInput.java
+++ src/arrayUserInput/ArrayUserInput.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /**
  * Name: Nigel Wilkerson
  * File: ArrayUserInput.java
- * Version: 1.0
+ * Version: 1.1
  * Date: 7/13/2026
  * Description: Demonstrates dynamically-sized arrays by asking the user how many movies to enter,
  *              then storing and displaying each movie
@@ -13,18 +13,15 @@ import java.util.Scanner;
 public class ArrayUserInput {
 
     static Scanner scanner = new Scanner(System.in);
-    static int numMovies;
+
 
     public static void main(String[] args) {
 
         // Declare variables
-        String[] watchedMovies;
-
-        // Call method Movies
-        getMovieCount();
+        int numMovies = getMovieCount();
 
         // This variable will equal the number of arrays needed from user-input
-        watchedMovies = new String[numMovies];
+        String[] watchedMovies = new String[numMovies];
 
         // Ask user the name of the movies they watched
         for(int i = 0; i < watchedMovies.length; i++) {
@@ -33,24 +30,26 @@ public class ArrayUserInput {
         }
 
         // Print the name of the movies the user watched
-        for(String movies : watchedMovies) {
-            System.out.printf("You watched %s this week.\n",movies);
+        for(String movie : watchedMovies) {
+            System.out.printf("You watched %s this week.\n",movie);
         }
         // Close scanner
         scanner.close();
     }
-    static void getMovieCount() {
+    static int getMovieCount() {
         // Ask user how many movies they watched
         System.out.print("How many movies have you watched this week? ");
-        numMovies = scanner.nextInt();
+        int count = scanner.nextInt();
         scanner.nextLine();
 
-        while (numMovies <= 0) {
+        while (count <= 0) {
             System.out.println("You must enter a positive number.");
             System.out.print("How many movies have you watched this week? ");
-            numMovies = scanner.nextInt();
+            count = scanner.nextInt();
             scanner.nextLine();
         }
+        return count;
     }
 
 }
+
